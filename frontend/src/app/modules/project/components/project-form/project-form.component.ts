7d710d2685c6142824bbe7b5b1b5c3f8e5c0c350
kstafa <kejsistafa2001@gmail.com>
2024-11-06 23:01:21 +0100

    add the project-form component

--- frontend/src/app/modules/project/components/project-form/project-form.component.ts
+++ frontend/src/app/modules/project/components/project-form/project-form.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { ProjectService } from '../../services/project.service';
+import { Project } from '../../models/project.model';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-project-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './project-form.component.html',
   styleUrl: './project-form.component.scss'
 })
-export class ProjectFormComponent {
 
+export class ProjectFormComponent {
+  projectForm!: FormGroup;
+
+  // TODO: hardcoded list for the categories
+  categories = ['Web', 'Mobile', 'Desktop'];
+
+  constructor(private fb: FormBuilder, private projectService: ProjectService, private router: Router) { }
+
+  // initialize the form with empty values
+  ngOnInit() {
+    this.projectForm = this.fb.group({
+      name: [''],
+      description: [''],
+      category: [''],
+      budget: ['']
+    })
+  }
+
+  // sumbit the form method 
+  onSubmit() {
+    // TODO: use other thing as id? 
+    const newProject: Project = {
+      id: Date.now(),
+      ...this.projectForm.value,
+    };
+
+    this.projectService.addProject(newProject);
+    this.router.navigate(['/projects']);
+  }
 }
