0e189a3cf125fd5ee434157f7a51807d7cde49e6
kstafa <kejsistafa2001@gmail.com>
2024-11-06 21:37:13 +0100

    added a service class for the projects

--- frontend/src/app/modules/project/services/project.service.ts
+++ frontend/src/app/modules/project/services/project.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { Project } from '../models/project.model';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
+  private projectsSubject: BehaviorSubject<Project[]> = new BehaviorSubject<Project[]>([]);
+  projects$: Observable<Project[]> = this.projectsSubject.asObservable();
 
   constructor() { }
+
+
+  //Get all projects
+  getProjects(): Observable<Project[]> {
+    return this.projects$;
+  }
+
+  //Add a new project
+  addProject(project: Project): void {
+    const projects = this.projectsSubject.getValue();
+    this.projectsSubject.next([...projects, project]);
+  }
+
+  //Update a project
+  updateProject(updateProject: Project): void {
+    const projects = this.projectsSubject.getValue().map((project) =>
+      project.id === updateProject.id ? updateProject : project
+    );
+    this.projectsSubject.next(projects);
+  }
+
+  //Delete a project
+  deleteProject(id: number): void {
+    const projects = this.projectsSubject.getValue().filter((project) => project.id !== id);
+    this.projectsSubject.next(projects);
+  }
 }
