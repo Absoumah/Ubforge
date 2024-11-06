e3701ee2e77a428f8a931872db3a734912dcd191
kstafa <kejsistafa2001@gmail.com>
2024-11-06 23:08:54 +0100

    defined the routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,18 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
 import { HelloWorldComponent } from './modules/hello-world/hello-world.component';
+import { ProjectListComponent } from './modules/project/components/project-list/project-list.component';
+import { ProjectFormComponent } from './modules/project/components/project-form/project-form.component';
 
 export const routes: Routes = [
+    // we'll keep here, for now
     { path: 'hello-world', component: HelloWorldComponent },
-    { path: '', redirectTo: '/hello-world', pathMatch: 'full' }
+
+    // prod routes
+    { path: 'projects', component: ProjectListComponent },
+    { path: 'projects/new', component: ProjectFormComponent },
+    { path: '', redirectTo: '/projects', pathMatch: 'full' }
+
+    // TODO: add a 404 page
 ];
