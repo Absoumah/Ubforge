2a219a185aad91b54caa415965ebe0f46d250ab3
kstafa <kejsistafa2001@gmail.com>
2024-11-09 21:11:55 +0100

    added the view for the assigned users

--- frontend/src/app/modules/project/components/project-item/project-item.component.ts
+++ frontend/src/app/modules/project/components/project-item/project-item.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserAvatarComponent } from '../user-avatar/user-avatar.component';
 import { Project } from '../../models/project.model';
 
 @Component({
   selector: 'app-project-item',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, UserAvatarComponent],
   templateUrl: './project-item.component.html',
   styleUrls: ['./project-item.component.scss']
 })
 export class ProjectItemComponent {
   @Input() project!: Project;
+  //TODO: change this ugly way of passing data (this should not came from here.)
+  @Input() assignedUsers: { initials: string }[] = ['A', 'B', 'C'].map(initials => ({ initials }));
   @Output() edit = new EventEmitter<number>();
   @Output() delete = new EventEmitter<number>();
 
