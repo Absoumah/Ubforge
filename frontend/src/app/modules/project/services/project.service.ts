eab90992c9d93cecfe3b66b7ae0a9ec79f79138b
kstafa <kejsistafa2001@gmail.com>
2024-11-09 21:20:02 +0100

    project service update

--- frontend/src/app/modules/project/services/project.service.ts
+++ frontend/src/app/modules/project/services/project.service.ts
@@ -11,24 +11,23 @@ export class ProjectService {
 
   constructor() { }
 
-
-  //Get all projects
+  // Get all projects
   getProjects(): Observable<Project[]> {
     return this.projects$;
   }
 
-  //Get a project by id
+  // Get a project by id
   getProjectById(id: number): Project | undefined {
     return this.projectsSubject.getValue().find((project) => project.id === id);
   }
 
-  //Add a new project
+  // Add a new project
   addProject(project: Project): void {
     const projects = this.projectsSubject.getValue();
     this.projectsSubject.next([...projects, project]);
   }
 
-  //Update a project
+  // Update a project
   updateProject(updateProject: Project): void {
     const projects = this.projectsSubject.getValue().map((project) =>
       project.id === updateProject.id ? updateProject : project
@@ -36,15 +35,15 @@ export class ProjectService {
     this.projectsSubject.next(projects);
   }
 
-  //Delete a project
+  // Delete a project
   deleteProject(id: number): void {
     const projects = this.projectsSubject.getValue().filter((project) => project.id !== id);
     this.projectsSubject.next(projects);
   }
 
-  //Get the categories 
-  //TODO: Hardcoded categories, should be fetched from the backend
-  //TODO: This method should be moved to a separate service
+  // Get the categories 
+  // TODO: Hardcoded categories, should be fetched from the backend
+  // TODO: This method should be moved to a separate service
   getCategories(): string[] {
     return ['Web', 'Mobile', 'Desktop', 'Cloud'];
   }
