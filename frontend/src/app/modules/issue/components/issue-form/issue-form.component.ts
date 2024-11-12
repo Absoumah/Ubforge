f6547ed01e5c9aa2a150d1aab7a2c235055e939b
kstafa <kejsistafa2001@gmail.com>
2024-11-12 01:20:48 +0100

    created the issue module and its components

---
+++ frontend/src/app/modules/issue/components/issue-form/issue-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './issue-form.component.html',
+  styleUrl: './issue-form.component.scss'
+})
+export class IssueFormComponent {
+
+}
