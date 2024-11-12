d0ddc15a0376c84118f9e0a86b89f8b7000a04f7
kstafa <kejsistafa2001@gmail.com>
2024-11-12 01:23:00 +0100

    defined the Isseus interface

--- frontend/src/app/modules/issue/models/issue.ts
+++ frontend/src/app/modules/issue/models/issue.ts
@@ -1,2 +1,17 @@
-export interface Issue {
+export interface Task {
+    id: number;
+    description: string;
+    completed: boolean;
 }
+
+export interface Issue {
+    id: number;
+    title: string;
+    category: string;
+    description: string;
+    reportedDate: Date;
+    dueDate: Date;
+    tasks: Task[];
+}
+
+export type IssueCategory = 'BugFix' | 'Feature' | 'Enhancement' | 'Documentation';
