54cba3eddc4be8f7e98836fa68e6736fb8fa3a5a
kstafa <kejsistafa2001@gmail.com>
2024-11-15 00:35:53 +0100

    basic component files for status-dropdwon

---
+++ frontend/src/app/shared/components/status-dropdown/status-dropdown.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-status-dropdown',
+  standalone: true,
+  imports: [],
+  templateUrl: './status-dropdown.component.html',
+  styleUrl: './status-dropdown.component.scss'
+})
+export class StatusDropdownComponent {
+
+}
