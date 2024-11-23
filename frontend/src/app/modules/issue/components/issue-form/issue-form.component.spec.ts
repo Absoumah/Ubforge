c54a39bf70406f294edd64d1ff3c42fca5769875
kstafa <kejsistafa2001@gmail.com>
2024-11-24 00:46:16 +0100

    basic task-form test

--- frontend/src/app/modules/issue/components/issue-form/issue-form.component.spec.ts
+++ frontend/src/app/modules/issue/components/issue-form/issue-form.component.spec.ts
@@ -1,16 +1,45 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter, ActivatedRoute, convertToParamMap } from '@angular/router';
 import { IssueFormComponent } from './issue-form.component';
+import { IssueService } from '../../services/issue.service';
+import { of } from 'rxjs';
+import { Issue } from '../../models/issue';
 
 describe('IssueFormComponent', () => {
   let component: IssueFormComponent;
   let fixture: ComponentFixture<IssueFormComponent>;
 
+  const mockIssue: Issue = {
+    id: 1,
+    title: 'Test Issue',
+    category: 'BugFix',
+    description: 'Test Description',
+    reportedDate: new Date(),
+    dueDate: new Date(),
+    tasks: []
+  };
+
+  const mockIssueService = {
+    getIssueById: jasmine.createSpy('getIssueById').and.returnValue(of(mockIssue))
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [IssueFormComponent]
+      imports: [IssueFormComponent],
+      providers: [
+        provideRouter([]),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: convertToParamMap({ id: '1' })
+            }
+          }
+        },
+        { provide: IssueService, useValue: mockIssueService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(IssueFormComponent);
     component = fixture.componentInstance;
