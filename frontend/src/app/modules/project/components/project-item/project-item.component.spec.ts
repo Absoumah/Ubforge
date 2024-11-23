60a5454e5e63b721d3cc07bb2fb2bc7845ce9637
kstafa <kejsistafa2001@gmail.com>
2024-11-24 00:22:36 +0100

    basic testing for project-item

--- frontend/src/app/modules/project/components/project-item/project-item.component.spec.ts
+++ frontend/src/app/modules/project/components/project-item/project-item.component.spec.ts
@@ -1,19 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProjectItemComponent } from './project-item.component';
+import { Project } from '../../models/project.model';
+import { User } from '../../../../core/models/user.model';
 
 describe('ProjectItemComponent', () => {
   let component: ProjectItemComponent;
   let fixture: ComponentFixture<ProjectItemComponent>;
 
+  const mockUser: User = {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+  };
+
+  const mockProject: Project = {
+    id: 1,
+    name: 'Test Project',
+    url: 'http://example.com',
+    category: 'Development',
+    description: 'Test Description',
+    assignedUsers: [mockUser]
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ProjectItemComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(ProjectItemComponent);
     component = fixture.componentInstance;
+
+    // Set required input
+    component.project = mockProject;
+
     fixture.detectChanges();
   });
 
