c72f5df24b801e7a49d46c4ba1c561ca9f8cd384
kstafa <kejsistafa2001@gmail.com>
2024-11-24 01:06:44 +0100

    basic task-detail test

--- frontend/src/app/modules/tasks/components/task-detail/task-detail.component.spec.ts
+++ frontend/src/app/modules/tasks/components/task-detail/task-detail.component.spec.ts
@@ -1,16 +1,46 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter, ActivatedRoute, convertToParamMap } from '@angular/router';
 import { TaskDetailComponent } from './task-detail.component';
+import { TaskService } from '../../services/task.service';
+import { of } from 'rxjs';
 
 describe('TaskDetailComponent', () => {
   let component: TaskDetailComponent;
   let fixture: ComponentFixture<TaskDetailComponent>;
 
+  const mockTask = {
+    id: 1,
+    name: 'Test Task',
+    description: 'Test Description',
+    status: 'OPEN',
+    priority: 'HIGH',
+    assignedTo: [],
+    estimatedHours: 8,
+    completed: false
+  };
+
+  const mockTaskService = {
+    getTaskById: jasmine.createSpy('getTaskById').and.returnValue(of(mockTask)),
+    updateTaskStatus: jasmine.createSpy('updateTaskStatus').and.returnValue(of({}))
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TaskDetailComponent]
+      imports: [TaskDetailComponent],
+      providers: [
+        provideRouter([]),
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: convertToParamMap({ id: '1' })
+            }
+          }
+        },
+        { provide: TaskService, useValue: mockTaskService }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(TaskDetailComponent);
     component = fixture.componentInstance;
