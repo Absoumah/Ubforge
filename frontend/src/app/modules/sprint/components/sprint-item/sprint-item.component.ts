6d9dc46d58b4558d9cf9bef4e7e6862c6c15ea2c
kstafa <kejsistafa2001@gmail.com>
2024-11-29 10:48:32 +0100

    added the sprint-item component

--- frontend/src/app/modules/sprint/components/sprint-item/sprint-item.component.ts
+++ frontend/src/app/modules/sprint/components/sprint-item/sprint-item.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { Sprint, SprintStatus } from '../../models/sprint.interface';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-sprint-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, DatePipe],
   templateUrl: './sprint-item.component.html',
-  styleUrl: './sprint-item.component.scss'
+  styleUrls: ['./sprint-item.component.scss']
 })
 export class SprintItemComponent {
+  @Input() sprint!: Sprint;
+  @Output() deleted = new EventEmitter<string>();
 
+  constructor(private router: Router) { }
+
+  get statusClass(): string {
+    return this.sprint.status.toLowerCase();
+  }
+
+  viewSprint(): void {
+    this.router.navigate(['/sprints', this.sprint.id]);
+  }
+
+  editSprint(event: Event): void {
+    event.stopPropagation();
+    this.router.navigate(['/sprints', this.sprint.id, 'edit']);
+  }
+
+  deleteSprint(event: Event): void {
+    event.stopPropagation();
+    this.deleted.emit(this.sprint.id);
+  }
 }
