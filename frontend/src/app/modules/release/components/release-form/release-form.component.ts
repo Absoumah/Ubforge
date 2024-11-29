cdae8b7d7cd4a1868c2133aa2af6281d05b8826b
kstafa <kejsistafa2001@gmail.com>
2024-11-29 09:11:10 +0100

    a first version for the release form

--- frontend/src/app/modules/release/components/release-form/release-form.component.ts
+++ frontend/src/app/modules/release/components/release-form/release-form.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ReleaseStatus } from '../../models/release';
 
 @Component({
   selector: 'app-release-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './release-form.component.html',
   styleUrl: './release-form.component.scss'
 })
-export class ReleaseFormComponent {
+export class ReleaseFormComponent implements OnInit {
 
+  releaseForm: FormGroup;
+  releaseStatuses = Object.values(ReleaseStatus);
+
+  constructor(private fb: FormBuilder) {
+    this.releaseForm = this.fb.group({
+      version: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      description: [''],
+      releaseDate: ['', [Validators.required]],
+      status: [ReleaseStatus.PLANNED, [Validators.required]],
+      projectId: ['', [Validators.required]]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    //TODO: Implement onSubmit method
+    console.log(this.releaseForm.value);
+  }
 }
--- frontend/src/app/modules/release/models/release.ts
+++ frontend/src/app/modules/release/models/release.ts
@@ -1,2 +1,17 @@
 export interface Release {
+    id: number; 
+    version: string;
+    name: string;
+    description: string;
+    releaseDate: string;
+    status: string;
+    projectId: number;
 }
+
+export enum ReleaseStatus {
+    PLANNED = 'PLANNED',
+    IN_PROGRESS = 'IN_PROGRESS',
+    RELEASED = 'COMPLETED', 
+    DELAYED = 'DELAYED'
+}
+
