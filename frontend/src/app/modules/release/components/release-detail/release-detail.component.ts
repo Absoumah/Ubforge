c4e7d349bd45b01aa44ee4401d6214b790f78704
kstafa <kejsistafa2001@gmail.com>
2024-11-29 09:53:23 +0100

    a detailed view for the releases

---
+++ frontend/src/app/modules/release/components/release-detail/release-detail.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-release-detail',
+  standalone: true,
+  imports: [],
+  templateUrl: './release-detail.component.html',
+  styleUrl: './release-detail.component.scss'
+})
+export class ReleaseDetailComponent {
+
+}
