401683624ae111acc3e22ae96b7f3f538be59757
kstafa <kejsistafa2001@gmail.com>
2024-11-29 10:34:50 +0100

    init structure for the sprints

---
+++ frontend/src/app/modules/sprint/components/sprint-form/sprint-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sprint-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './sprint-form.component.html',
+  styleUrl: './sprint-form.component.scss'
+})
+export class SprintFormComponent {
+
+}
