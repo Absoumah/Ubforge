354cfa875bd654ee53a326ec65e3f206922b5597
kstafa <kejsistafa2001@gmail.com>
2024-11-29 10:45:08 +0100

    added the sprints to the sidebar

--- frontend/src/app/modules/sprint/components/sprint-list/sprint-list.component.ts
+++ frontend/src/app/modules/sprint/components/sprint-list/sprint-list.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SprintItemComponent } from '../sprint-item/sprint-item.component';
 import { SprintService } from '../../services/sprint.service';
+import { ProjectStateService } from '../../../project/services/project-state.service';
 import { Sprint } from '../../models/sprint.interface';
+import { Subscription, switchMap } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sprint-list',
@@ -11,15 +14,44 @@ import { Sprint } from '../../models/sprint.interface';
   templateUrl: './sprint-list.component.html',
   styleUrls: ['./sprint-list.component.scss']
 })
-export class SprintListComponent implements OnInit {
+export class SprintListComponent implements OnInit, OnDestroy {
   sprints: Sprint[] = [];
+  private subscription?: Subscription;
 
-  constructor(private sprintService: SprintService) { }
+  constructor(
+    private sprintService: SprintService,
+    private projectStateService: ProjectStateService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
     this.loadSprints();
   }
 
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
   private loadSprints(): void {
+    this.subscription = this.projectStateService.getActiveProjectId()
+      .pipe(
+        switchMap(projectId => {
+          if (!projectId) {
+            return [];
+          }
+          return this.sprintService.getSprintsByProject(projectId.toString());
+        })
+      )
+      .subscribe(sprints => {
+        this.sprints = sprints;
+      });
+  }
+
+  createSprint(): void {
+    this.router.navigate(['/sprints/create']);
+  }
+
+  onSprintDeleted(sprintId: string): void {
+    this.sprintService.deleteSprint(sprintId);
   }
 }
