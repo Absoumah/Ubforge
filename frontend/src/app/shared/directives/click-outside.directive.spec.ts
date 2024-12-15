e190a1e8b04302b9eeed476afa9b2b35cf5efee8
kstafa <kejsistafa2001@gmail.com>
2024-12-15 20:02:44 +0100

    added the tests for the most of the project files

--- frontend/src/app/shared/directives/click-outside.directive.spec.ts
+++ frontend/src/app/shared/directives/click-outside.directive.spec.ts
@@ -2,9 +2,34 @@ import { ElementRef } from '@angular/core';
 import { ClickOutsideDirective } from './click-outside.directive';
 
 describe('ClickOutsideDirective', () => {
+  let directive: ClickOutsideDirective;
+  let elementRefMock: ElementRef;
+
+  beforeEach(() => {
+    elementRefMock = { nativeElement: document.createElement('div') } as ElementRef;
+    directive = new ClickOutsideDirective(elementRefMock);
+  });
+
   it('should create an instance', () => {
-    const elementRefMock = { nativeElement: document.createElement('div') };
-    const directive = new ClickOutsideDirective(elementRefMock as ElementRef);
     expect(directive).toBeTruthy();
+  });
+
+  it('should emit when clicking outside element', () => {
+    const outsideElement = document.createElement('button');
+    spyOn(directive.clickOutside, 'emit');
+    
+    directive.onClick(outsideElement);
+
+    expect(directive.clickOutside.emit).toHaveBeenCalled();
+  });
+
+  it('should not emit when clicking inside element', () => {
+    const insideElement = document.createElement('button');
+    elementRefMock.nativeElement.appendChild(insideElement);
+    spyOn(directive.clickOutside, 'emit');
+    
+    directive.onClick(insideElement);
+
+    expect(directive.clickOutside.emit).not.toHaveBeenCalled();
   });
 });
